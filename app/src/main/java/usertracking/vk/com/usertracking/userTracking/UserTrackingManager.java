78fd5670d5f4d40a86baf92d07ca2550f26da008
Vinay <vinaykashyapts@gmail.com>
2017-11-28 21:06:00 +0100

    Tracking manager singleton added

---
+++ app/src/main/java/usertracking/vk/com/usertracking/userTracking/UserTrackingManager.java
@@ -0,0 +1,41 @@
+package usertracking.vk.com.usertracking.userTracking;
+
+import android.content.Context;
+
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+
+/**
+ * Created by Vinay on 28/11/17.
+ */
+
+public class UserTrackingManager {
+
+    private static final String TAG = UserTrackingManager.class.getSimpleName();
+
+    private FirebaseJobDispatcher dispatcher;
+
+    private static UserTrackingManager mInstance;
+    private static Context mContext;
+
+    private UserTrackingManager() {
+    }
+
+    public static synchronized UserTrackingManager getInstance(Context context) {
+        if (null == mInstance) {
+            mInstance = new UserTrackingManager();
+            mContext = context;
+
+        }
+
+        return mInstance;
+    }
+
+    public void startTracking() {
+        
+    }
+
+    public void stopTracking() {
+
+    }
+}
+
